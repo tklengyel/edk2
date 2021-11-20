e43a55c461b754d766c841353742d9bb159b181d
Steven <steven.shi@intel.com>
2021-11-20 21:46:30 +0800

    Implement ThunkPrint() over the EmuThunk Ppi to output debug info Add GuardCountExecuted member in EMU_THUNK_PROTOCOL to record total executed guard counts

--- MdeModulePkg/Library/LibSimLib/SanitizerCoverage.c
+++ MdeModulePkg/Library/LibSimLib/SanitizerCoverage.c
@@ -14,19 +14,112 @@
 
 
 #include <Uefi.h>
+#include <PiPei.h>
 //#include <Library/SanitizerCoverage.h>
 #include <Library/DebugLib.h>
+#include <Library/PeiServicesLib.h>
+#include <Library/PrintLib.h>
+#include <Library/EmuMagicPageLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Ppi/EmuThunk.h>
 
 VOID
 SerialOutput(
   IN  CONST CHAR8      *String
   );
 
+EMU_THUNK_PROTOCOL     *Emu_Thunk = NULL;
+UINT64                 *Guard_Executed = NULL;
+/**
+  This service enables PEIMs to discover a given instance of an interface.
+
+  So this is, well a hack, so we can reuse the same libraries as the PEI Core
+  for XIP modules....
+
+  @param  Guid                  A pointer to the GUID whose corresponding interface needs to be
+                                found.
+  @param  Instance              The N-th instance of the interface that is required.
+  @param  PpiDescriptor         A pointer to instance of the EFI_PEI_PPI_DESCRIPTOR.
+  @param  Ppi                   A pointer to the instance of the interface.
+
+  @retval EFI_SUCCESS           The interface was successfully returned.
+  @retval EFI_NOT_FOUND         The PPI descriptor is not found in the database.
+
+**/
+EFI_STATUS
+EFIAPI
+LocateEmuThunkPpi (
+  IN CONST EFI_GUID             *Guid,
+  IN UINTN                      Instance,
+  IN OUT EFI_PEI_PPI_DESCRIPTOR **PpiDescriptor,
+  IN OUT VOID                   **Ppi
+  )
+{
+  EFI_PEI_PPI_DESCRIPTOR *PpiList;
+
+  if (Instance != 0) {
+    return EFI_NOT_FOUND;
+  }
+
+  for (PpiList = EMU_MAGIC_PAGE()->PpiList; ; PpiList++) {
+    if (CompareGuid (PpiList->Guid, Guid)) {
+      if (PpiDescriptor != NULL) {
+        *PpiDescriptor = PpiList;
+      }
+      if (Ppi != NULL) {
+        *Ppi = PpiList->Ppi;
+      }
+      return EFI_SUCCESS;
+    }
+
+    if ((PpiList->Flags & EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST) == EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST) {
+      break;
+    }
+  }
+
+
+  return EFI_NOT_FOUND;
+}
+
 VOID
-SecPrint (
+ThunkPrint (
   CHAR8  *Format,
   ...
-  );
+  )
+{
+  VA_LIST                   Marker;
+  UINTN                     CharCount;
+  CHAR8                     Buffer[0x1000];
+  EFI_STATUS                Status;
+  EMU_THUNK_PPI             *ThunkPpi;
+
+  if (Emu_Thunk == NULL){
+      //
+      // Locate EmuThunkPpi for retrieving standard output handle
+      //
+      Status = LocateEmuThunkPpi (
+                  &gEmuThunkPpiGuid,
+                  0,
+                  NULL,
+                  (VOID **) &ThunkPpi
+                 );
+      if (!EFI_ERROR (Status)) {
+        Emu_Thunk  = (EMU_THUNK_PROTOCOL *)ThunkPpi->Thunk ();
+      }
+  }
+
+  if (Emu_Thunk){
+      //
+      // Convert message to ASCII String and print out
+      //
+      VA_START (Marker, Format);
+      AsciiVSPrint (Buffer, sizeof(Buffer), Format, Marker);
+      VA_END (Marker);
+      CharCount = AsciiStrLen(Buffer);
+      Emu_Thunk->WriteStdErr ((UINT8 *)Buffer, CharCount);
+  }
+}
+
 
 extern UINT64 __start___sancov_cntrs;
 extern UINT8 __stop___sancov_cntrs;
@@ -61,18 +154,21 @@ void __sanitizer_cov_trace_pc_guard_init(UINT32* start, UINT32* stop) {
 
   static UINT64 N;  // Counter for the guards.
   if (start == stop || *start) return;  // Initialize only once.
-  DEBUG ((EFI_D_ERROR, "__sanitizer_cov_trace_pc_guard_init, start=0x%x, stop=0x%x\n",start, stop));
+  //DEBUG ((EFI_D_ERROR, "__sanitizer_cov_trace_pc_guard_init, start=0x%x, stop=0x%x\n",start, stop));
 
   for (UINT32 *x = start; x < stop; x++)
     *x = ++N;  // Guards should start from 1.
 
-  DEBUG ((EFI_D_ERROR, "Total BB Count= 0x%x\n", N));
+  ThunkPrint ("Total BB Count= 0x%x\n", N);
 }
 
 // clang only support no_sanitize("coverage") attribute if
 // compiler-rt project is enabled when build LLVM/Clang compiler
 // __attribute__((no_sanitize("coverage")))
 void __sanitizer_cov_trace_pc_guard(UINT32* guard) {
+  EFI_STATUS                Status;
+  EMU_THUNK_PPI             *ThunkPpi;
+
   if (!*guard) return;
 
   //void *PC = __builtin_return_address(0);
@@ -82,7 +178,27 @@ void __sanitizer_cov_trace_pc_guard(UINT32* guard) {
   //__sanitizer_symbolize_pc(PC, "%p %F %L", PcDescr, sizeof(PcDescr));
   //printf("guard: %p %x PC %s\n", guard, *guard, PcDescr);
   //DEBUG ((EFI_D_ERROR, "guard=0x%x, *guard=0x%x, PC=0x%x\n",guard, *guard, PC));
-  DEBUG ((EFI_D_ERROR, "BB:0x%x\n", *guard));
+  //ThunkPrint ("guard:0x%x *guard:0x%x PC:0x%x\n", guard, *guard, PC);
+  ThunkPrint ("BB:0x%x\n", *guard);
+
+  if (!Guard_Executed){
+      //
+      // Locate EmuThunkPpi for retrieving standard output handle
+      //
+      Status = LocateEmuThunkPpi (
+                  &gEmuThunkPpiGuid,
+                  0,
+                  NULL,
+                  (VOID **) &ThunkPpi
+                 );
+      if (!EFI_ERROR (Status)) {
+        Emu_Thunk  = (EMU_THUNK_PROTOCOL *)ThunkPpi->Thunk ();
+        Guard_Executed = &Emu_Thunk->GuardCountExecuted;
+        (*Guard_Executed)++;
+      }
+  } else{
+      (*Guard_Executed)++;
+  }
 }
 
 // clang only support no_sanitize("coverage") attribute if
